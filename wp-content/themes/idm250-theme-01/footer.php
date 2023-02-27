b496e5632595812f7658127b3fa97f9630159843
nytrea <anoushsan@gmail.com>
2023-02-27 11:07:15 -0500

    update footer css

--- wp-content/themes/idm250-theme-01/footer.php
+++ wp-content/themes/idm250-theme-01/footer.php
@@ -1,6 +1,7 @@
 </main>
 <footer>
-    <p> Earthen Goods &copy; <?php echo date('Y'); ?> </p>
+    <h1 class="footer-title"> Earthen </h1>
+    <h1 class="footer-descriptor"> Handmade Goods &copy; <?php echo date('Y'); ?> </h1>
 </footer>
 <?php wp_footer(); ?>
 </body>
