2961b5affcfecc9e41c73e916ef6c3910d353d62
nytrea <anoushsan@gmail.com>
2023-03-06 00:11:57 -0500

    update single.php to condense content

---
+++ wp-content/themes/idm250-theme-01/page-43.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<?php get_template_part('components/content'); ?>
+
+<?php get_footer(); ?>
--- wp-content/themes/idm250-theme-01/single.php
+++ wp-content/themes/idm250-theme-01/single.php
@@ -6,29 +6,29 @@
 
     <h1><?php echo get_the_title(); ?></h1>
     <div><?php echo get_the_excerpt(); ?></div>
+    <!-- <h2> </h2> -->
 
-    <div class="blog-categories">
-    <h2> Categories for this post </h2>
+    <!-- <div class="blog-categories">
+    <h2> Categories for this post </h2> -->
 
 
-        <?php
-    $currentPostID = get_the_id();
-    $terms = get_the_terms($currentPostID, 'category');
+        <!-- <?php
+    // $currentPostID = get_the_id();
+    // $terms = get_the_terms($currentPostID, 'category');
 
-    if ($terms){
-        foreach ($terms as $term){
-            echo "<span>{$term->name}</span>";
-        }
-    }
+    // if ($terms){
+    //     foreach ($terms as $term){
+    //         echo "<span>{$term->name}</span>";
+    //     }
+    // }
     ?>
     </div>
     <?php
 
     //checks if the post has a thumbnail
-    if (has_post_thumbnail()) {
-    the_post_thumbnail();
-    }
-    ?>
+    // if (has_post_thumbnail()) {
+    // the_post_thumbnail();
+    // }
+    ?> -->
 
-<?php get_template_part('components/content'); ?>
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
