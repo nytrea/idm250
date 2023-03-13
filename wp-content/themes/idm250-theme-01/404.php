578c0d9228ee587092aa5231d6e305695759a7ce
nytrea <anoushsan@gmail.com>
2023-03-13 01:45:15 -0400

    footer to bottom

--- wp-content/themes/idm250-theme-01/404.php
+++ wp-content/themes/idm250-theme-01/404.php
@@ -2,6 +2,9 @@
 
 <div>
     <!-- any content to denote it's a 404 error page -->
+    <h2> 404 </h2>
+    <img>
+    <p>The Page You’re Looking For Doesn’t Seem To Exist.</p>
 </div>
 
 <?php get_footer(); ?>
