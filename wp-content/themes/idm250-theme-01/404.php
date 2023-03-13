cdf98148a2e6a394bd27fe457a89cdf1e31e3a94
nytrea <anoushsan@gmail.com>
2023-03-13 18:36:49 -0400

    fix 404 footer

--- wp-content/themes/idm250-theme-01/404.php
+++ wp-content/themes/idm250-theme-01/404.php
@@ -2,10 +2,10 @@
 
 <div>
     <!-- any content to denote it's a 404 error page -->
-    <h2> 404 </h2>
-    <!-- <img src="../dist/images/broken-ceramic.png"> -->
-    <p>The Page You’re Looking For Doesn’t Seem To Exist.</p>
+    <h2 class="error-title"> 404 </h2>
+    <img src="/wp-content/themes/idm250-theme-01/dist/images/broken-ceramic.png" class="error-img">
+    <p class="error-message">The Page You’re Looking For Doesn’t Seem To Exist.</p>
 </div>
 
-<?php get_footer(); ?>
+<?php get_footer('simple'); ?>
 
---
+++ wp-content/themes/idm250-theme-01/footer-simple.php
@@ -0,0 +1,9 @@
+
+</main>
+<foote class="error-footer">
+    <!-- <h1 class="footer-title"> Earthen </h1> -->
+    <h1 class="footer-descriptor"> Earthen Handmade Goods | For Educational Purposes &copy;  <?php echo date('Y'); ?> </h1>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
