b0be0e302cb28aa93e5c02a943ad177ecfb64146
nytrea <anoushsan@gmail.com>
2023-03-21 19:29:56 -0400

    date formatting

--- wp-content/themes/idm250-theme-01/components/blog-card-horizontal.php
+++ wp-content/themes/idm250-theme-01/components/blog-card-horizontal.php
@@ -1,16 +1,14 @@
 <article class="relative isolate flex flex-col gap-8 lg:flex-row">
   <div class="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
-    <img src="<?php echo get_the_post_thumbnail_url(); ?>"
-      alt="<?php echo get_the_title(); ?> Featured Image"
-      class="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover">
+    <a href="<?php echo get_the_permalink(); ?>">
+        <img src="<?php echo get_the_post_thumbnail_url(); ?>"
+        alt="<?php echo get_the_title(); ?> Featured Image"
+        class="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover blog-listing-image">
+      </a>
     <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
   </div>
   <div>
-    <div class="flex items-center gap-x-4 text-xs">
-      <time datetime="<?php echo get_the_date(); ?>"
-        class="text-gray-500"><?php echo get_the_date(); ?></time>
-      <?php get_template_part('components/blog-categories-list'); ?>
-    </div>
+
     <div class="group relative max-w-xl">
       <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
         <a href="<?php echo get_the_permalink(); ?>">
@@ -24,5 +22,11 @@
       </p>
       <?php endif; ?>
     </div>
+
+    <div class="flex items-center gap-x-4 blog-listing-date">
+      <time datetime="<?php echo get_the_date(); ?>"
+        class="text-gray-500 text-center"><?php echo get_the_date(); ?></time>
+      <?php get_template_part('components/blog-categories-list'); ?>
   </div>
+
 </article>
