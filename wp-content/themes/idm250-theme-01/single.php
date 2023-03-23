077744679c1c8a3dfec686311884dc79b1e788c7
nytrea <anoushsan@gmail.com>
2023-03-23 15:37:24 -0400

    blog post hero images

--- wp-content/themes/idm250-theme-01/single.php
+++ wp-content/themes/idm250-theme-01/single.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
+    <div class="overlay">
+    <div class="hero-image-container" style="background-image: url(
+        <?php echo get_the_post_thumbnail_url(); ?>
+        )">
+    </div>
+    </div>
+
     <div class="article-text-container">
         <article id='post-<?php the_ID(); ?>' <?php post_class(); ?>>
             <?php get_template_part("components/content"); ?>
